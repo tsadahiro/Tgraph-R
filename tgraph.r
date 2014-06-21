6ad2bf2b0e8e398686d66d2b913cc1559329a7cd
Taizo Sadhiro <sadahiro@taizox201.(none)>
2014-06-21 11:01:27 +0900

    small change

--- tgraph.r
+++ tgraph.r
@@ -10,16 +10,40 @@ rectKasteleyne <- function(m,n){ # m*n must be even
     colnames(K) <- wn
 
     # determine white vertices on the boundary
-    xb1 <- seq(2,m, by=2)
-    if (m <= 2){
-        xb2 <- c()
-    }else{
-        xb2 <- seq(m-1-((m+n)%%2),1,-2)
+
+    xb <- c()
+    yb <- c()
+    x <- 1
+    y <- 1
+    while (x < m){
+        if ((x + y)%%2 == 1){
+            xb <- c(xb,x)
+            yb <- c(yb,y)
+        }
+        x <- x+1
     }
-    yb1 <- seq(2+(m+1)%%2,n,2)
-    yb2 <- seq(n-1-(n+1)%%2,1,-2)
-    xb <- c(xb1,rep(m,length(yb1)), xb2, rep(1,length(yb2)))
-    yb <- c(rep(1,length(xb1)), yb1, rep(n,length(xb2)),yb2)
+    while (y < n){
+        if ((x + y)%%2 == 1){
+            xb <- c(xb,x)
+            yb <- c(yb,y)
+        }
+        y <- y+1
+    }
+    while (x > 1){
+        if ((x + y)%%2 == 1){
+            xb <- c(xb,x)
+            yb <- c(yb,y)
+        }
+        x <- x-1
+    }
+    while (y > 1){
+        if ((x + y)%%2 == 1){
+            xb <- c(xb,x)
+            yb <- c(yb,y)
+        }
+        y <- y-1
+    }
+
     whiteboundary <- paste(xb, yb, sep=",")
     
     lab2coord <- function(s){
@@ -43,20 +67,61 @@ rectKasteleyne <- function(m,n){ # m*n must be even
     return(list(K=K,xboundary=xb, yboundary=yb))
 }
 
-m <- 4
-n <- 3
-KK <- rectKasteleyne(m,n)
-K <- KK$K
-v <- rep(0,dim(K)[1])
-v[1] <- 1
-Kd <- K %*% diag(as.vector(solve(K) %*% matrix(v,ncol=1)))
-colnames(Kd) <- colnames(K)
-rownames(Kd) <- rownames(K)
-vw <- rep(0,dim(K)[1])
-names(vw) <- colnames(K)
-blab <- paste(KK$xboundary, KK$yboundary, sep=",")
-vw[blab] <- diff(c(1+1i,KK$xboundary + KK$yboundary*1i))
-Kt <- diag(as.vector(matrix(vw,nrow=1) %*% solve(Kd)) ) %*% Kd
-colnames(Kt) <- colnames(K)
-rownames(Kt) <- rownames(K)
+gauge <- function(m,n){
+    KK <- rectKasteleyne(m,n)
+    K <- KK$K
+    v <- rep(0,dim(K)[1])
+    v[1] <- 1
+    Kd <- K %*% diag(as.vector(solve(K) %*% matrix(v,ncol=1)))
+    colnames(Kd) <- colnames(K)
+    rownames(Kd) <- rownames(K)
+    vw <- rep(0,dim(K)[1])
+    names(vw) <- colnames(K)
+    blab <- paste(KK$xboundary, KK$yboundary, sep=",")
+    vw[blab] <- diff(c(1+1i,KK$xboundary + KK$yboundary*1i))
+    Kt <- diag(as.vector(matrix(vw,nrow=1) %*% solve(Kd)) ) %*% Kd
+    colnames(Kt) <- colnames(K)
+    rownames(Kt) <- rownames(K)
+    return(list(m=m, n=n,K=Kt, xboundary=KK$xboundary, yboundary=KK$yboundary))
+}
 
+Kintegral <- function(KK){
+    m <- KK$m
+    n <- KK$n
+
+    xb <- c()
+    yb <- c()
+    x <- 1
+    y <- 1
+    while (x < m){
+        if ((x + y)%%2 == 0){
+            xb <- c(xb,x)
+            yb <- c(yb,y)
+        }
+        x <- x+1
+    }
+    while (y < n){
+        if ((x + y)%%2 == 0){
+            xb <- c(xb,x)
+            yb <- c(yb,y)
+        }
+        y <- y+1
+    }
+    while (x > 1){
+        if ((x + y)%%2 == 0){
+            xb <- c(xb,x)
+            yb <- c(yb,y)
+        }
+        x <- x-1
+    }
+    while (y > 1){
+        if ((x + y)%%2 == 0){
+            xb <- c(xb,x)
+            yb <- c(yb,y)
+        }
+        y <- y-1
+    }
+        
+    return(cbind(xb,yb))
+    
+}
